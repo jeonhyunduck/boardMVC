fe2a3bc4a343cf8505af4d972364d1c471beb565
jeonhyunduck <blied1004@naver.com>
2020-11-12 10:07:14 +0900

    스프링 초기 셋팅
    마이마티스 적용
    마리아 버전 10.4.12
    스프링 버전 4.2.0.RELEASE

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/be/e056f354f723001b13ed97ddb6dc7f81
@@ -0,0 +1,5 @@
+package com.web.board.service;
+
+public class BoardService {
+
+}
---
+++ board/src/main/java/com/web/board/vo/BoardValueObject.java
@@ -0,0 +1,73 @@
+package com.web.board.vo;
+
+public class BoardValueObject {
+	private int boardId;
+
+	private String boardTitle;
+
+	private String boardContents;
+
+	private String createUser;
+
+	private String createDate;
+
+	private String updateUser;
+
+	public int getBoardId() {
+		return boardId;
+	}
+
+	public void setBoardId(int boardId) {
+		this.boardId = boardId;
+	}
+
+	public String getBoardTitle() {
+		return boardTitle;
+	}
+
+	public void setBoardTitle(String boardTitle) {
+		this.boardTitle = boardTitle;
+	}
+
+	public String getBoardContents() {
+		return boardContents;
+	}
+
+	public void setBoardContents(String boardContents) {
+		this.boardContents = boardContents;
+	}
+
+	public String getCreateUser() {
+		return createUser;
+	}
+
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
+	}
+
+	public String getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getUpdateUser() {
+		return updateUser;
+	}
+
+	public void setUpdateUser(String updateUser) {
+		this.updateUser = updateUser;
+	}
+
+	public String getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	private String updateDate;
+}
